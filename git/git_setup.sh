f7bb2088974cbc1c448376c41eb849a94d07e0b9
masayu1724 <sgymuiaa1724@gmail.com>
2016-12-04 22:24:03 +0900

    2016年 12月  4日 日曜日 22:24:03 JST

--- git/git_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source $MY_CONF
-
-git config --global user.name ${GIT['user.name']}
-git config --global user.email ${GIT['user.email']}
-
-git config --global core.editor "/usr/bin/vim"
-git config --global color.ui true
